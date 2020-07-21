1db6add0264be9b68439e55d1cb7476abf1d01a4
JAYDEN Y H XIE (ITA-ISDC-ISD-OOCLL/ZHA) <XIEJA3@OOCL.COM>
2020-07-21 20:46:13 +0800

    1 [jayden] feature: print receipt

--- README.md
+++ README.md
@@ -1,72 +1,66 @@
-# Requirement
-A cash register (POS) system is used in the store for settlement of the store. This cashier will settle and print the receipt (Receipt) according to the item(Item) in the customer's shopping cart (Cart) at the time of settlement.
+# Tasking
 
-We need to implement a function called printReceipt, which can input the data of the specified format as a parameter and then output the text of the receipt in the browser console.
+- ### 1. 统计每个item的个数
+  * 输入：barcodes：[string]
+  * 输出：cartItems：[{itemCode:string,quantiy:int}]
 
-This time, the input will be an array of barcodes (string). For example:
-```javascript
-[
-  'ITEM000000',
-  'ITEM000000',
-  'ITEM000000',
-  'ITEM000000',
-  'ITEM000000',
-  'ITEM000001',
-  'ITEM000001',
-  'ITEM000004'
-]
-```
+- ### 2. 获取每个item的详细信息
+  * 输入：cartItems：[{itemCode:string,quantiy:int}]
+  * 输出：cartItemDetails：[{itemCode:string,name:string,quantiy:int,unitPrice:int}]
+  
+- ### 3. 计算每个item的总价格
+  * 输入： cartItemDetails：[{itemCode:string,name:string,quantiy:int,unitPrice:int}]
+  * 输出：cartItemDetailsWithTotalPrice：[{itemCode:string,name:string,quantiy:int,unitPrice:int,totalPrice:int}]
 
-Then the output should be 
-```
-***<store earning no money>Receipt ***
-Name: Coca-Cola, Quantity: 5, Unit price: 3 (yuan), Subtotal: 15 (yuan)
-Name: Sprite, Quantity: 2, Unit price: 3 (yuan), Subtotal: 6 (yuan)
-Name: Battery, Quantity: 1, Unit price: 2 (yuan), Subtotal: 2 (yuan)
-----------------------
-Total: 23 (yuan)
-**********************
-```
+- ### 4. 计算所有item的总价
+  * 输入：cartItemDetailsWithTotalPrice：[{itemCode:string,name:string,quantiy:int,unitPrice:int,totalPrice:int}]
+  * 输出：totalPrice
 
-Suppose that our database is as follows:
-```javascript
-[
-   {
-      barcode: 'ITEM000000',
-      name: 'Coca-Cola',
-      price: 3
-    },
-    {
-      barcode: 'ITEM000001',
-      name: 'Sprite',
-      price: 3
-    },
-    {
-      barcode: 'ITEM000002',
-      name: 'Apple',
-      price: 5
-    },
-    {
-      barcode: 'ITEM000003',
-      name: 'Litchi',
-      price: 15
-    },
-    {
-      barcode: 'ITEM000004',
-      name: 'Battery',
-      price: 2
-    },
-    {
-      barcode: 'ITEM000005',
-      name: 'Instant Noodles',
-      price: 4
-    }
-]
-```
+- ### 5. 格式化数据
+  * 输入：cartItemDetailsWithTotalPrice：[{itemCode:string,name:string,quantiy:int,unitPrice:int,totalPrice:int}]，totalPrice
+  * 输出：receipt:string
 
-# Principal
+- ### 6. 输出数据
+  * 输入：receipt
+  * 输出：console.log(receipt)
 
-1. Please draw context diagram.
-2. Please declare all the methods according to your diagram.
-3. Please implement the function according to the context diagram
-4. Please repeat step 3 until all functions are implemented.
+
+!['receipt_contextMap'](./receipt_contextMap.png)
+
+  
+caluItemNum()|Description
+--|:--:|--:
+P|5min
+D|12min
+C|在js中，忘记怎么获取相同字符串的个数和对数组去重
+A|需要多加练习这些方法
+
+getItemDetails()|Description
+--|:--:|--:
+P|3min
+D|10min
+C|难在合并两个对象
+A|使用了Object.assign合并两个对象
+
+caluEachItemTotalPrice()|Description
+--|:--:|--:
+P|3min
+D|2min
+C|这个只用一个foreach循环，然后得出quantity * price的总价格
+A|这个比较简单
+
+caluAllItemTotalPrice()|Description
+--|:--:|--:
+P|2min
+D|2min
+C|这个只用一个foreach循环，然后得出每个item的totalPrice的和
+A|这个比较简单
+
+fomatReceipt()|Description
+--|:--:|--:
+P|5min
+D|5min
+C|这个知识拼接字符串，相对简单
+A|暂时没什么难点
+
+  
--- main.js
+++ main.js
@@ -1,13 +1,120 @@
-function printReceipt(barcodes) {
-    console.log(`
-***<store earning no money>Receipt ***
-Name: Coca-Cola, Quantity: 5, Unit price: 3 (yuan), Subtotal: 15 (yuan)
-Name: Sprite, Quantity: 2, Unit price: 3 (yuan), Subtotal: 6 (yuan)
-Name: Battery, Quantity: 1, Unit price: 2 (yuan), Subtotal: 2 (yuan)
-----------------------
-Total: 23 (yuan)
-**********************`)
+function dataBase() {
+    let data =  [
+        {
+           barcode: 'ITEM000000',
+           name: 'Coca-Cola',
+           price: 3
+         },
+         {
+           barcode: 'ITEM000001',
+           name: 'Sprite',
+           price: 3
+         },
+         {
+           barcode: 'ITEM000002',
+           name: 'Apple',
+           price: 5
+         },
+         {
+           barcode: 'ITEM000003',
+           name: 'Litchi',
+           price: 15
+         },
+         {
+           barcode: 'ITEM000004',
+           name: 'Battery',
+           price: 2
+         },
+         {
+           barcode: 'ITEM000005',
+           name: 'Instant Noodles',
+           price: 4
+         }
+     ];
+     return data;
 }
+
+
+function printReceipt(barcodes) {
+    let cartItems = caluItemNum(barcodes);
+    let cartItemDetails = getItemDetails(cartItems);
+    let cartItemDetailsWithTotalPrice = caluEachItemTotalPrice(cartItemDetails);
+    let totalPrice = caluAllItemTotalPrice(cartItemDetailsWithTotalPrice);
+    let receipt = fomatReceipt(cartItemDetailsWithTotalPrice, totalPrice);
+    console.log(receipt);
+}
+
+function caluItemNum(barcodes) {
+    let cartItems = [];
+    let barcode = '';
+    let hasCode = '';
+    for (let index = 0; index < barcodes.length; index++) {
+        if(hasCode.indexOf(barcodes[index])>=0) {
+            continue;
+        }
+        hasCode += barcodes[index];
+        barcode = barcodes[index];
+        let quantity = 0;
+        barcodes.forEach(e => {
+            if(e===barcode) {
+                quantity++;
+            }
+        });
+        cartItems.push({barcode:barcode,quantity:quantity});
+    }
+    return cartItems;
+}
+
+function getItemDetails(cartItems) {
+    let cartItemDetails = [];
+    let itemData = dataBase();
+    cartItems.forEach(item => {
+        let data = itemData.find((data) => 
+            item.barcode === data.barcode
+        )
+        item = Object.assign(item, data);
+        cartItemDetails.push(item);
+    });
+    return cartItemDetails;
+}
+
+function caluEachItemTotalPrice(cartItemDetails) {
+    cartItemDetails.forEach(e => {
+        e.totalPrice = e.quantity * e.price;
+    });
+    return cartItemDetails;
+}
+
+function caluAllItemTotalPrice(cartItemDetailsWithTotalPrice) {
+    let totalPrice = 0;
+    cartItemDetailsWithTotalPrice.forEach(e => {
+        totalPrice = totalPrice + e.totalPrice;
+    });
+    return totalPrice;
+}
+
+function fomatReceipt(cartItemDetailsWithTotalPrice, totalPrice) {
+    let receiptInfo = '';
+    let receipt = '';
+    for (let i = 0; i < cartItemDetailsWithTotalPrice.length; i++) {
+        const e = cartItemDetailsWithTotalPrice[i];
+        receiptInfo = receiptInfo + `Name: ${e.name}, Quantity: ${e.quantity}, Unit price: ${e.price} (yuan), Subtotal: ${e.totalPrice} (yuan)`;
+        if(i < cartItemDetailsWithTotalPrice.length-1) {
+            receiptInfo += '\n';
+        }
+    }
+    receipt = `
+***<store earning no money>Receipt ***
+${receiptInfo}
+----------------------
+Total: ${totalPrice} (yuan)
+**********************`;
+    
+    return receipt;
+
+  
+}
+
 
 
 module.exports = {
